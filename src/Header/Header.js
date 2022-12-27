f8b44f97db8b96f75aa53e19a9d84e4ae5c8a54b
Vaidik Singh Nirwan <vaidiksinghnirwan.com>
2022-12-28 00:54:19 +0530

    added the about us page as responsive for the mobile and laptop

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {FaTimes} from 'react-icons/fa'
 
 
 const Header = () => {
@@ -13,12 +14,24 @@ const Header = () => {
         </div>
         <div className='header-details'>
             <ul>
-                <li><a><Link to={'/'}>Home</Link></a></li>
-                <li><a><Link to={'/aboutUs'}>AboutUS</Link></a></li>
-                <li><a><Link to={'/contactUs'}>ContactUS</Link></a></li>
-                <li><a><Link to={'/community'}>Community</Link></a></li>
-                <li><a><Link to={'/news'}>News</Link></a></li>
+                <li><Link to={'/'}>Home</Link></li>
+                <li><Link to={'/aboutUs'}>AboutUS</Link></li>
+                <li><Link to={'/contactUs'}>ContactUS</Link></li>
+                <li><Link to={'/community'}>Community</Link></li>
+                <li><Link to={'/news'}>News</Link></li>
             </ul>
+        </div>
+
+        <div className="mobile-view">
+            <ul>
+                <li><Link to={'/'}>Home</Link></li>
+                <li><Link to={'/aboutUs'}>AboutUS</Link></li>
+                <li><Link to={'/contactUs'}>ContactUS</Link></li>
+                <li><Link to={'/community'}>Community</Link></li>
+                <li><Link to={'/news'}>News</Link></li>
+            </ul>
+
+            <button><FaTimes/></button>
         </div>
     </div>
   )
--- src/Main/AboutUs.js
+++ src/Main/AboutUs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Fade,Transform} from 'react-animation-components'
+// import {Fade,Transform} from 'react-animation-components';
 
 const AboutUs = () => {
   return (
@@ -9,7 +9,7 @@ const AboutUs = () => {
       </div>
       <div className="about-content">
         <p>
-          PixelExperience is an AOSP based ROM, with Google apps included and all Pixel goodies (launcher, wallpapers, icons, fonts, bootanimation).Our mission is to offer the maximum possible stability and security, along with essential and useful features for the proper functioning of the device.We are a community-driven open source project, so we have no affiliation with Google LLC company.
+          PixelExperience is an AOSP based ROM, with Google apps included and all Pixel goodies (launcher, wallpapers, icons, fonts, bootanimation). Our mission is to offer the maximum possible stability and security, along with essential and useful features for the proper functioning of the device.We are a community-driven open source project, so we have no affiliation with Google LLC company.
         </p>
       </div>
     </div>
